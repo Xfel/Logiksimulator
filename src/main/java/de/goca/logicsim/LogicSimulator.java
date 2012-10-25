30709f2f8f8546aa11857442cf8201e191338c56
Xfel <felixtreede@yahoo.de>
2012-10-25 22:30:01 +0200

    Hauptklasse erstellt un in Maven eingetragen

---
+++ src/main/java/de/goca/logicsim/LogicSimulator.java
@@ -0,0 +1,17 @@
+package de.goca.logicsim;
+
+/**
+ * Main class and entry point.
+ * 
+ * @author Felix Treede
+ *
+ */
+public class LogicSimulator {
+	/**
+	 * Application entry point
+	 * @param args launch arguments
+	 */
+	public static void main(String[] args) {
+		// TODO write code
+	}
+}
